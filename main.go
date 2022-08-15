0937049519f2177dd95abb4addb1ee7ccad19c28
fami0719 <fmitinaga238@gmail.com>
2022-08-15 18:09:11 +0900

    add width depth option

--- main.go
+++ main.go
@@ -22,6 +22,11 @@ var favicon = []byte{
 
 func main() {
 
+	outputPath := flag.String("o", "./image.png", "Path to output file")
+
+	width := flag.Int("w", 200, "Width of the output image of the QR Code (px)")
+	depth := flag.Int("d", 200, "height of the output image of the QR Code (px)")
+
 	flag.Parse()
 	url := flag.Arg(0)
 	if url == "" {
@@ -29,14 +34,14 @@ func main() {
 		return
 	}
 
-	file, err := os.Create("./qr.png")
+	file, err := os.Create(*outputPath)
 	if err != nil {
 		fmt.Printf("file generation failed: %v\n", err)
 		return
 	}
 	defer file.Close()
 
-	img, err := qrgen.GenQRCode(url)
+	img, err := qrgen.GenQRCode(url, *width, *depth)
 	if err != nil {
 		fmt.Printf("image generation failed: %\n", err)
 		return
